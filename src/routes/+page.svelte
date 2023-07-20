0acebca0c2825f03518e0b04ad326d20dd355c23
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-20 18:29:29 -0400

    feat: add a daisyui button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a class="" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="btn btn-primary">Button</button>
