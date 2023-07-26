1acb5647b7837532d5c14ee3f982702dbe55a309
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-26 00:06:38 -0400

    feat: add auth through google

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+    import { auth } from "$lib/firebase";
+    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-    export let data: PageData;
+    async function signInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        const user = await signInWithPopup(auth, provider);
+
+        console.log(user);
+    }
 </script>
+
+<h2 class="text-3xl text-center font-bold">Login</h2>
+<button class="btn btn-circle btn-primary btn-wide" on:click={signInWithGoogle}>Sign in with Google</button>
