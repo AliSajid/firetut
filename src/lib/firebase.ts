ed3199c4cad9823ee737f843f9076d7f7b05b19d
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-26 22:37:00 -0400

    feat: add auth state change store

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCWg-n9BFPXAeNMrj0nlh-obovBActGmG8", // pragma: allowlist secret
@@ -17,3 +18,35 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+/**
+ * Creates a writable store that tracks the current user's authentication state using the Firebase Authentication library.
+ * If Firebase Auth is not initialized or we are rendering on the server, returns a store with a null value.
+ * @return {Object} An object with a `subscribe` method that can be used to subscribe to changes in the user's authentication state.
+ * The `subscribe` method takes a callback function as its argument, which is called whenever the value of the store changes.
+ * The callback function is passed the new value of the store as its argument.
+ */
+function userStore(): { subscribe: (callback: (value: User | null) => void) => void } {
+
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn("Firebase auth not available");
+    const { subscribe } = writable<User | null>(null);
+    return { subscribe };
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  }
+}
+
+export const user = userStore();
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-    import { auth } from "$lib/firebase";
-    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+    import { auth, user } from "$lib/firebase";
+    import { GoogleAuthProvider, signInWithPopup, signOut} from "firebase/auth";
 
-    async function signInWithGoogle() {
+/**
+ * Signs in the user using Google OAuth authentication.
+ * @return {Promise<void>} A promise that resolves with the user credential object if the sign-in is successful.
+ * The user credential object contains information about the signed-in user, such as their user ID and access token.
+ * If the sign-in is unsuccessful, the promise is rejected with an error object that contains information about the error.
+ */
+async function signInWithGoogle(): Promise<void> {
         const provider = new GoogleAuthProvider();
         const user = await signInWithPopup(auth, provider);
 
@@ -11,4 +17,11 @@
 </script>
 
 <h2 class="text-3xl text-center font-bold">Login</h2>
+
+{#if $user}
+<h2 class="card-title">Welcome, {$user.displayName}</h2>
+<p class="text-center text-success">You are successfully logged in</p>
+<button class="btn btn-circle btn-danger btn-wide" on:click={() => signOut(auth)}>Sign out</button>
+{:else}
 <button class="btn btn-circle btn-primary btn-wide" on:click={signInWithGoogle}>Sign in with Google</button>
+{/if}
