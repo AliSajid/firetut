8935a1ebb824f5dd5cc775346217050ad714875d
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-26 22:59:22 -0400

    feat: add AuthCheck component

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { user } from "$lib/firebase";
+</script>
+
+{#if $user}
+<slot></slot>
+{:else}
+    <p class="text-error">
+        You are not authorized to view this page. Please sign in.
+        <a class="btn btn-primary btn-wide" href="/login">Sign in</a>
+    </p>
+{/if}
